ee526b9e7304ca22481d1f82bc0d8977b4d9ba96
Andrew Gordineer <gordineerandrew@gmail.com>
2015-03-17 16:56:22 -0500

    'Fixed the terrible guardian angels minor whoopsies: implemented shuffling after initializing the deck. Added a draw method so that players can draw from the deck'

--- Deck.java
+++ Deck.java
@@ -1,18 +1,29 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class Deck{
-    ArrayList<Card> deck = new ArrayList<Card>(52);
-    private int values = 13;
-    private int suits = 4;
+    final static int DECKSIZE = 52;
+    private ArrayList<Card> deck = new ArrayList<Card>(DECKSIZE);
+    /* total number of suits represented within a deck */
+    final static int SUITS = 4;
+    /* total number of values represented within a deck */
+    final static int VALUES = 13;
+    /* total number of times to shuffle the deck before it is sufficiently shuffled */
+    final static int SHUFFLE_NUM = 7;
+
+    private int deckIndex;
 
     public Deck(){
-        int deckIndex = 0;
+
+        /* fill the deck with cards in initial sorted order */
+        deckIndex = 0;
         Card.Suit suit = Card.Suit.HEART;
-        for(int i = 0; i < this.suits; i++){
-            for(int j = 0; j < this.values; j++){
+        for(int i = 0; i < SUITS; i++){
+            for(int j = 0; j < VALUES; j++){
                 Card card = new Card(suit, intToValue(j));
                 deck.set(deckIndex, card);
+                deckIndex++;
             }
             if(i == 0){
                 suit = Card.Suit.DIAMOND;
@@ -22,6 +33,22 @@ public class Deck{
                 suit = Card.Suit.CLUB;
             }
         }
+
+        /* reset the deckIndex to the top of the deck */
+        deckIndex = 0;
+
+
+        /* shuffle the deck */
+        for(int i = 0; i < SHUFFLE_NUM; i++)
+            Collections.shuffle(deck);
+    }
+
+    /* draws from the deck if there are cards to draw */
+    public Card draw(){
+        if(deckIndex == DECKSIZE)
+            return null;
+
+        return deck.get(deckIndex++);
     }
 
     private static Card.Value intToValue(int value){
