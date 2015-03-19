94005cee53b7b8cb45c69bf0cbdd38c5808cb917
Scott Munro <scottmunro@utexas.edu>
2015-03-19 13:21:50 -0500

    fairly large commit to GoFish. read rest of commit msg for full deets CHANGES: * added an all_player list so that the all of the players can be operated on at once POLYMORPHISM TO THE RESCUE
    * added the 'throws InterruptedException' stuff to main and another static function because any function that will cause the program to sleep needs to include that.
    
    * created a chooseStartingPlayer routine so that when it is time for the game to start each player will roll a dice and the player with the largest soonest dice roll will go first. Routine is pretty much explained within comments and is fairly simple so no need to discuss details of it here

--- GoFish.java
+++ GoFish.java
@@ -1,8 +1,8 @@
 /*
 ## GameDriver
-* shuffle deck at start of game (shuffle 7 times for true shuffle)
-* deal 7 cards to each player
-* auto collect any books.
+DONE * shuffle deck at start of game (shuffle 7 times for true shuffle)
+DONE * deal 7 cards to each player
+DONE * auto collect any books.
 * select player to take the first turn at random
 ### Game Loop
 * specify the current players turn
@@ -24,15 +24,17 @@
 /* imported utilities */
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class GoFish{
 
     /* GLOBAL CONSTANTS */
     private static final int MAX_OPPONENTS = 3;
     private static final int STARTING_HAND = 7;
+    private static final int HALF_SEC = 500;
     private static final boolean DEBUG = true;
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException{
         /*
         GAME SETUP
         */
@@ -52,13 +54,23 @@ public class GoFish{
         /* create the deck */
         Deck deck = new Deck();
 
+        /* list of all players
+        room for all of the opponents and the human player*/
+        ArrayList<Player> all_players = new ArrayList<Player>(numOpponents+1);
+
         /* generate human player */
         HumanPlayer user = new HumanPlayer("Player 1", deck);
+        /* add to all player list to keep track of user */
+        all_players.add(user);
 
         /* generate opponents */
         ArrayList<AIPlayer> opponents = new ArrayList<AIPlayer>(numOpponents);
         for(int i = 0; i < numOpponents; i++){
-            opponents.add(new AIPlayer("Opponent "+(i+1), deck));
+            AIPlayer opp = new AIPlayer("Opponent "+(i+1), deck);
+            /* add to both opplist and alllist so
+            that the new opponent can be kept track of */
+            opponents.add(opp);
+            all_players.add(opp)
         }
 
         /* deal 7 cards to each players */
@@ -72,10 +84,19 @@ public class GoFish{
             opp.displayState();
         }
 
+        /* choose player to begin game */
+        /* current_player stores the index of the player in the alllist */
+        int current_player = chooseStartingPlayer(all_players);
+        System.out.println("%s will begin the game");
+        System.out.println("Press ENTER to let the game begin");
+        userIn.next();
 
+        /* begin game loop */
 
     }
 
+    /* static routine that deals cards out to each player
+    at the beginning of each game */
     public static void dealCards(Player p){
         for(int i = 0; i < STARTING_HAND; i++){
             Card c = p.drawCard();
@@ -85,5 +106,40 @@ public class GoFish{
 
         if(DEBUG)
             System.out.println();
+    }
+
+    /* chooses the starting player by having each player
+    roll a random die. The player with the highest roll
+    is the player who starts */
+    public static int chooseStartingPlayer(ArrayList<Player> playerlist) throws InterruptedException{
+        /* keeps track of the largest roll */
+        int max_roll = 0;
+        /* keeps track of the player with the largest roll */
+        int starting_player = -1;
+        /* a random number generate to generate the dice rolls */
+        Random r = new Random();
+
+        System.out.println("Rolling dice...");
+        /* each player will roll a dice
+        the player with the highest dice roll will go first */
+        for(int i = 0; i < num_players){
+            /* wait for half a second to give player time to read info */
+            Thread.sleep(HALF_SEC);
+
+            /* roll a number 1 - 6 */
+            int players_roll = r.nextInt()+1;
+            if(players_roll > max_roll){
+                max_roll = players_roll;
+                starting_player = i;
+            }
+
+            /* display the player's roll */
+            System.out.printf("%s rolled a... \t%d\n", playerlist.get(i), players_roll);
+        }
+
+        Thread.sleep(HALF_SEC);
+
+        assert starting_player != -1;
+        return starting_player;
     }
 }
